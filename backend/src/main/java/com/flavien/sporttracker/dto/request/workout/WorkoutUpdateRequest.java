59106dd5bec1004e2edbffad1c54a35224b29c1b
Flavien <flaviensuman@gmail.com>
2026-03-25 11:00:04 +0100

    problème de remise à null pour datePlanned reglé lors de l'update + factoriastion css pour message erreur forms

--- backend/src/main/java/com/flavien/sporttracker/dto/request/workout/WorkoutUpdateRequest.java
+++ backend/src/main/java/com/flavien/sporttracker/dto/request/workout/WorkoutUpdateRequest.java
@@ -3,7 +3,9 @@ package com.flavien.sporttracker.dto.request.workout;
 import java.time.LocalDate;
 import java.util.Optional;
 
+import org.openapitools.jackson.nullable.JsonNullable;
+
 public record WorkoutUpdateRequest(
-                Optional<String> name,
-                Optional<LocalDate> datePlanned) {
+        Optional<String> name,
+        JsonNullable<LocalDate> datePlanned) {
 }
